14d1cee990489b6908615590896d803573834e44
megarex5000 <harrisonchoquepuma@gmail.com>
2025-09-10 18:17:35 -0400

    Estructura inicial
    se agregaron
    src.- aqui va el codigo fuente principal
    models.- aqui las clases
    data.-aqui los archivos de datos
    docs.-documentacion del proyecto

---
+++ models/Especie.h
@@ -0,0 +1,17 @@
+#ifndef ESPECIE_H
+#define ESPECIE_H
+
+#include <string>
+
+class Especie {
+public:
+    int id;
+    std::string nombre;
+    std::string categoria;
+    std::string habitat;
+    int poblacion;
+
+    void mostrar();
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+int main() {
+    std::cout << "Proyecto Animales en Peligro - Bolivia\n";
+    return 0;
+}
